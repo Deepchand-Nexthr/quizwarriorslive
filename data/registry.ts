00cc334efbf39cb86fbb6c9e3b2800c43a320eba
Deep <deepc774@gmail.com>
2026-06-12 03:41:04 +0530

    Disable PMP and BA quizzes

--- data/registry.ts
+++ data/registry.ts
@@ -1,13 +1,9 @@
 import { quizData as staffing001 } from "./careers/staffing/staffing-001";
-import { quizData as pmp001 } from "./careers/pmp/pmp-001";
-import { quizData as ba001 } from "./careers/ba/ba-001";
 import { quizData as sf001 } from "./careers/salesforceadmin/salesforceadmin-001";
-import { quizData as ramayan001 } from "./india/ramayan-001";
+import { quizData as ramayan001 } from "./ramayan-001";
 
 export const quizRegistry = {
   "staffing-001": staffing001,
-  "pmp-001": pmp001,
-  "ba-001": ba001,
   "salesforceadmin-001": sf001,
   "ramayan-001": ramayan001,
 };
